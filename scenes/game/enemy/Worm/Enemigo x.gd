4fc61519961392f82e2878c6bbb9fd652e041cda
Yisus0108 <wwwtito030@gmail.com>
2024-12-01 23:43:57 -0500

    Update Enemigo x.gd

--- scenes/game/enemy/Worm/Enemigo x.gd
+++ scenes/game/enemy/Worm/Enemigo x.gd
@@ -1,9 +1,11 @@
 extends CharacterBody2D
 
-var velocidad: int = 100
+var velocidad: int = 50
 
 func _ready():
 	velocity.x =velocidad
 
 func _physics_process(delta):
+	if is_on_wall():
+		pass
 	move_and_slide()
