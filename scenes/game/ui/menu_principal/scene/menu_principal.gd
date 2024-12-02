014f4e439c247bf19b4da1ebbd93a1744a0142ce
Rosa1512 <fa.re.sa1512@gmail.com>
2024-12-02 09:40:28 -0500

    Menu en un 90% falta unir al escenario

--- scenes/game/ui/menu_principal/scene/menu_principal.gd
+++ scenes/game/ui/menu_principal/scene/menu_principal.gd
@@ -1,11 +1,12 @@
 extends Node
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
+func _on_button_pressed():
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _on_button_2_pressed():
+	pass # Replace with function body.
+
+
+func _on_button_3_pressed():
+	pass # Replace with function body.
